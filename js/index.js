5f58fc5e4863f1f2cb9af74f222616cdc0cddc76
Iury1978 <advent777@yandex.ru>
2023-05-04 17:22:26 +0300

    slider

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const swiperNew = new Swiper(".swiper_new", {
+  slidesPerView: 4,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".block-slider__next_new",
+    prevEl: ".block-slider__prev_new",
+  },
+});
+
+const swiperHits = new Swiper(".swiper_hits", {
+  slidesPerView: 4,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".block-slider__next_hits",
+    prevEl: ".block-slider__prev_hits",
+  },
+});
